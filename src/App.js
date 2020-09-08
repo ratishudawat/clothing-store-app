7fed9731c0e3e8cd7a1831fb6752e394f455dd56
Ratish Udawat <ratishudawat@gmail.com>
2020-09-09 02:07:35 +0530

    sign in page created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.compenent';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
