163f567524647631b62c0f64de5998d84eb30c0b
Ratish Udawat <ratishudawat@gmail.com>
2020-09-18 08:55:25 +0530

    added redux persist lib for cart data persistence

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './store/store';
+
+import {store, persistor} from './store/store';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
 
 import logger from 'redux-logger';
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-export default store;
+ const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ const persistor = persistStore(store);
+
+export { store, persistor };
